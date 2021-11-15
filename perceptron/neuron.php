6c7d824208bae1e9034500eef6112366016d44da
WampServer <linuxphp123@gmail.com>
2021-11-16 00:00:00 +0330

    perceptron

---
+++ perceptron/neuron.php
@@ -0,0 +1,14 @@
+<?php
+
+// inputs
+$x = array(2, 3);
+// weights
+$w = array(0.7, 0.6);
+//bias
+$b = 1;
+
+$sum = $x[0] * $w[0] + $x[1] * $w[1] + $b;
+
+// ReLU Activation Function
+if ($sum > 0) {echo $sum;} else {echo 0;}
+?>
